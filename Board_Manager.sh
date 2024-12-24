151405c19742be691b9e755261ad8bf96e9b0d38
derczaja <derczaja123@gmail.com>
2024-12-24 14:24:37 +0100

    added board update functionality to Board_Manager.sh

--- Board_Manager.sh
+++ Board_Manager.sh
@@ -2,21 +2,19 @@ echo "Willkommen bei Czaja's Board Manager!"
 echo ""
 echo "Was willst du tun?"
 echo "(1) Darscheibe Nutzer wechseln"
-echo "(2) Darscheibe ausschalten"
+echo "(2) Dartscheibe updaten"
+echo "(3) Darscheibe ausschalten"
 read -n 1 -p "Deine Auswahl: " x
 echo ""
 echo ""
 
-
-if [[ "$x" != "1" && "$x" != "2" ]]; then
-    echo "UngÃ¼ltiger Eingabewert! Das Programm wird geschlossen."
-    exit 1
-fi
-
 if [[ "$x" == "1" ]]; then
     sshpass -p 'odroid' ssh root@192.168.178.43 '/root/setup_board.sh'
-fi
-
-if [[ "$x" == "2" ]]; then
+elif [[ "$x" == "2" ]]; then
+    sshpass -p 'odroid' ssh root@192.168.178.43 '/root/update_board.sh'
+elif [[ "$x" == "3" ]]; then
     sshpass -p 'odroid' ssh root@192.168.178.43 '/root/board_shutdown.sh'
+else
+    echo "UngÃ¼ltiger Eingabewert! Das Programm wird geschlossen."
+    exit 1
 fi
