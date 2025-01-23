69c8d18c6048b112ba69d6f4b65a821901553a7f
Philip Czaja <derczaja123@gmail.com>
2025-01-23 23:13:01 +0100

    fixed path to config.toml added the missing /home/... in the beginning of the path in the setup_board.sh

--- OnBoardScripts/setup_board.sh
+++ OnBoardScripts/setup_board.sh
@@ -13,14 +13,14 @@ if [[ "$x" != "1" && "$x" != "2" ]]; then
 fi
 
 if [[ "$x" == "1" ]]; then
-    sed -i "s/board_id = '.*'/board_id = '7ab070a3-daf5-4310-aa16-c74f91e5443a'/" /czaja/.config/autodarts/config.toml
-    sed -i "s/api_key = '.*'/api_key = 'HffAeKLyNaUPERuJDxlYSZAeUr98kx5D'/" /czaja/.config/autodarts/config.toml
+    sed -i "s/board_id = '.*'/board_id = '7ab070a3-daf5-4310-aa16-c74f91e5443a'/" /home/czaja/.config/autodarts/config.toml
+    sed -i "s/api_key = '.*'/api_key = 'HffAeKLyNaUPERuJDxlYSZAeUr98kx5D'/" /home/czaja/.config/autodarts/config.toml
     printf "Die Dartscheibe wurde erfolgreich fuer Hugo konfiguriert!\n"
 fi
 
 if [[ "$x" == "2" ]]; then
-    sed -i "s/board_id = '.*'/board_id = 'b3dfde8d-d426-442a-bd56-68a611becd80'/" /czaja/.config/autodarts/config.toml
-    sed -i "s/api_key = '.*'/api_key = 'PbLqfAccqTRfVpbTabJwS9Vf4l4qT4T2'/" /czaja/.config/autodarts/config.toml
+    sed -i "s/board_id = '.*'/board_id = 'b3dfde8d-d426-442a-bd56-68a611becd80'/" /home/czaja/.config/autodarts/config.toml
+    sed -i "s/api_key = '.*'/api_key = 'PbLqfAccqTRfVpbTabJwS9Vf4l4qT4T2'/" /home/czaja/.config/autodarts/config.toml
     printf "Die Dartscheibe wurde erfolgreich fuer Philip konfiguriert!\n"
 fi
 
